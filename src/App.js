1ceb3352652e89f76e947cccaedc84deae4a7ad2
Andrea Ercoli <andrea.ercoli@madisoft.it>
2021-06-16 15:01:30 +0200

    Static grid from configuration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import "./App.css";
 import { NGrid } from "./NGrid/NGrid";
 
 function App() {
-  return <NGrid />;
+  return <NGrid columns={columns} />;
 }
+
+const columns = [
+  // {
+  // title: "Selezione",
+  // formatter: "rowSelection",
+  // titleFormatter: "rowSelection",
+  // frozen: true
+  // },
+  { title: "Nome", field: "name" },
+  { title: "Anno di nascita", field: "birthYear" },
+  { title: "Specie", field: "species.name" },
+  { title: "Pianeta specie", field: "species.homeworld.name" },
+  { title: "Pianeta", field: "homeworld.name" },
+  { title: "Genere", field: "gender" },
+  { title: "Peso", field: "mass" },
+  { title: "Colore occhi", field: "eyeColor" },
+];
 
 export default App;
--- src/NGrid/NGrid.js
+++ src/NGrid/NGrid.js
@@ -1,3 +1,28 @@
-export function NGrid() {
-  return <h1>NGrid</h1>
+import get from "lodash/get";
+import { peopleData } from "../api/data";
+import "./variables.css";
+import "./ngrid.css";
+
+export function NGrid({ columns }) {
+  const { people } = peopleData;
+  return (
+    <table role="grid" className="ngrid">
+      <thead>
+        <tr>
+          {columns.map((col) => (
+            <th key={col.title}>{col.title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {people.map((row, i) => (
+          <tr key={i}>
+            {columns.map((col, k) => (
+              <td key={k}>{get(row, col?.field ?? "")}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
